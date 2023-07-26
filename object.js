7d0007cbccb2237dd81f413bf613eaa7a75b1ead
md tarek hossain <hossaintarek944@gmail.com>
2023-07-26 20:24:16 +0600

    object added

---
+++ object.js
@@ -0,0 +1,21 @@
+var student = {
+    name: 'Tarek hossain',
+    phone: '01731****83',
+    id: 12,
+    class: 3
+
+}
+
+
+var computer ={
+    ram: 3,
+    hardisk: 500,
+    brand: 'dell'
+}
+console.log(computer);
+console.log(student.phone);
+console.log(computer)
+computer.ram= 5;
+console.log(computer);
+computer.brand='Lenavo'
+console.log(computer)
