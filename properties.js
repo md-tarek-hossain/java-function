b287896e2b454950e3449fda3f0802243785f468
md tarek hossain <hossaintarek944@gmail.com>
2023-07-27 10:49:08 +0600

    java-propertis

---
+++ properties.js
@@ -0,0 +1,11 @@
+var shopingCart ={
+    book: 3,
+    sunglasses:1,
+    keybord: 5,
+    mous :7,
+
+}
+var propertiesName = Object.keys(shopingCart);
+var pvalue =Object.values(shopingCart)
+console.log(pvalue)
+console.log(propertiesName)
